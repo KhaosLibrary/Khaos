b5c750b40508a38f3c5e9bcddac21ca453ad77bf
David Cole <dc@sourcetutor.com>
2015-09-06 12:18:13 +0100

    improve the khaos/fsm test suite

---
+++ src/Khaos/FSM/tests/Test/BacktrackingRunnerTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Test\Khaos\FSM;
+
+use Khaos\FSM\BacktrackingRunner;
+use Khaos\FSM\Context;
+use Khaos\FSM\DefaultState;
+use Khaos\FSM\State;
+use PHPUnit_Framework_TestCase;
+
+class BacktrackingRunnerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var BacktrackingRunner
+     */
+    private $fsm;
+
+    protected function setUp()
+    {
+        $s1 = new DefaultState('S1', State::TYPE_INITIAL);
+        $s2 = new DefaultState('S2');
+        $s3 = new DefaultState('S3');
+        $s4 = new DefaultState('S4');
+        $s5 = new DefaultState('S5', State::TYPE_TERMINAL);
+
+        $s1->addTransition('t1', $s2);
+        $s1->addTransition('t1', $s3);
+        $s2->addTransition('t2', $s4);
+        $s3->addTransition('t2', $s5);
+
+        $this->fsm = new BacktrackingRunner($s1, new Context());
+    }
+
+    /**
+     * @test
+     */
+    public function it_finds_the_correct_path_to_reach_a_terminal_state_given_valid_input()
+    {
+        $this->assertEquals(['S3', 'S5'], $this->fsm->input(['t1', 't2']));
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_false_given_invalid_input()
+    {
+        $this->assertEquals(false, $this->fsm->input(['t1', 't3']));
+    }
+
+    /**
+     *
+     */
+    public function it_must_end_on_a_terminal_state_to_return_result()
+    {
+        $this->assertEquals(['S3', 'S5'], $this->fsm->input(['t1', 't2']));
+        $this->assertEquals(false, $this->fsm->input(['t1']));
+    }
+}
