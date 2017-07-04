0ac4a9be3263dd58420b60099f397954fa2b8a4f
David Cole <dcole@inviqa.com>
2017-07-04 19:09:33 +0100

    wip

--- src/Khaos/Schema/whiteboard.php
+++ src/Khaos/Schema/whiteboard.php
@@ -1,21 +1,31 @@
 <?php
 
 use Khaos\Schema\KeywordCollection;
+use Khaos\Schema\Keywords\PropertiesKeyword;
 use Khaos\Schema\Keywords\TypeKeyword;
 use Khaos\Schema\SchemaInstanceValidator;
 
 require_once __DIR__.'/../../../vendor/autoload.php';
 
 $schema = [
-    'type' => 'string'
+    'type' => 'object',
+    'properties' => [
+        'command' => ['type' => 'string'],
+        'usage'   => ['type' => 'string']
+    ]
 ];
 
-$instance = "123";
+$instance = [
+    'command' => 'help',
+    'usage'   => 'bench help'
+];
 
 
 $keywords = new KeywordCollection();
 $keywords->add(new TypeKeyword());
+$keywords->add(new PropertiesKeyword());
 
 $validator = new SchemaInstanceValidator($keywords);
 
 var_dump($validator->validate($schema, $instance));
+var_dump($instance);
